90df983e076f71b584451199d0b21065d3e4c3a3
Mateus Irineu Guimarães Mallmann <mateusmallmann2013@gmail.com>
2025-03-25 11:43:22 -0300

    Start project

---
+++ backend/src/main/java/com/example/manutencao_equipamentos/ManutencaoEquipamentosApplication.java
@@ -0,0 +1,13 @@
+package com.example.manutencao_equipamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManutencaoEquipamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManutencaoEquipamentosApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/manutencao_equipamentos/ManutencaoEquipamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.manutencao_equipamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManutencaoEquipamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
