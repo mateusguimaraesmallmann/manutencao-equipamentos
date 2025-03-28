317cf7d177093c8a677f312dfa31bdc0177712d4
Eduardo <edubello93@gmail.com>
2025-03-28 18:04:04 -0300

    iniciado rf003

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeClienteComponent } from './paginas/home-cliente/home-cliente.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: 'cliente',
+        component: HomeClienteComponent,
+        
+      },
+
+];
