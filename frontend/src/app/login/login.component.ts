be15e1212c3f7be76c0db41b3c5017385d2a82f9
Montaguti <5656679+Montaguti@users.noreply.github.com>
2025-04-02 02:16:30 -0300

    feat when clicking on signin it makes an http request to localhost:3000/login

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -2,6 +2,8 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+import { AuthService } from '../services/auth.service';
+
 @Component({
   selector: 'app-login',
   standalone: true,
@@ -13,8 +15,18 @@ export class LoginComponent {
   email: string = "";
   password: string = "";
 
+  constructor(private authService: AuthService) { }
+
   onSubmit() {
-    alert("SIGNIN CLICKED");
+    this.authService.login(this.email, this.password).subscribe({
+      next: (response) => {
+        console.log("success signIn: ", response)
+        console.log("USER LOGGED IN!");
+      },
+      error: (err) => {
+        console.error("Error during signIn", err);
+      }
+    })
   }
 
   onSignup() {
