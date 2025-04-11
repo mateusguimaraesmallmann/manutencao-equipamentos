d4f78cd6d5c885038286d536e3eeef189b4e16e9
Montaguti <5656679+Montaguti@users.noreply.github.com>
2025-04-11 17:01:18 -0300

    fix navigation on navbar-cliente

--- frontend/src/app/components/navbar-cliente/navbar-cliente.component.ts
+++ frontend/src/app/components/navbar-cliente/navbar-cliente.component.ts
@@ -1,16 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar-cliente',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './navbar-cliente.component.html',
   styleUrl: './navbar-cliente.component.css'
 })
 export class NavbarClienteComponent {
-
-
   logout() {
   }
-
 }
