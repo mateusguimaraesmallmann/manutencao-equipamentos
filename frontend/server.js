33afe7eeebebdc2da371eeaa9e218d3d7f01b4dd
Montaguti <5656679+Montaguti@users.noreply.github.com>
2025-04-13 22:56:38 -0300

    fix json-server fake returning profile on login

--- frontend/server.js
+++ frontend/server.js
@@ -12,13 +12,19 @@ server.post("/login", (req, res) => {
 	const users = router.db.get("users").value(); // Returns the user object
 	const user = users.find((u) => u.email === email);
 	if (user) {
-		res.json({
+		const result_json = {
 			id: user.id,
 			name: user.name,
 			email: user.email,
 			token: `mock-token-${user.id}`,
 			role: user.role,
-		});
+			profile: user.profile,
+		};
+
+		if (user.role == 1) result_json["employee_id"] = user.employee_id;
+		if (user.role == 2) result_json["client_id"] = user.client_id;
+
+		res.json(result_json);
 	} else {
 		res.status(401).json({
 			message: "Invalid email or password",
