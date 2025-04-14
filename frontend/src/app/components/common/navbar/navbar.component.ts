6bbaa33d6f2738a6398c37c1c452fd3a27176cfb
Montaguti <5656679+Montaguti@users.noreply.github.com>
2025-04-13 21:39:32 -0300

    fix login component moved to components folder added user model in order to autheticate properly db.json returning the users with role login form with error messages login form using purely bootstrap

--- frontend/src/app/components/common/navbar/navbar.component.ts
+++ frontend/src/app/components/common/navbar/navbar.component.ts
@@ -1,18 +1,41 @@
 import { Component } from '@angular/core';
-import { NgFor } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
 	selector: 'dynamic-navbar',
 	standalone: true,
-	imports: [RouterModule, NgFor],
+	imports: [],
 	templateUrl: './navbar.component.html',
 	styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
-	client_menu_items = [{ name: 'Clientes', path: '/cliente' }];
+	private name: string | undefined;
+	private auth: AuthService;
+	private router: Router;
+
+	constructor(auth: AuthService, router: Router) {
+		this.name = auth.getUser()?.name;
+		this.router = router;
+		this.auth = auth;
+	}
+
+	client_menu_items = [
+		{ name: 'Clientes', path: '/cliente' },
+		{ name: 'Equipamentos', path: '/equipamento' },
+	];
 
 	getItems(): Array<any> {
 		return this.client_menu_items;
+	}
+
+	getName(): string {
+		return this.name || 'INVALID USER';
+	}
+
+	logout(): void {
+		this.auth.logout();
+		this.router.navigate(['/login']);
 	}
 }
