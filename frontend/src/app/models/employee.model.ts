7e7bab9451ebf8b5a5660e6de24371a257333b25
Montaguti <5656679+Montaguti@users.noreply.github.com>
2025-04-23 12:32:34 -0300

    fix employee model to conform requirements

--- frontend/src/app/models/employee.model.ts
+++ frontend/src/app/models/employee.model.ts
@@ -3,10 +3,7 @@ import { User } from '.';
 export class Employee {
 	constructor(
 		public id: Number | null,
-		public document: string,
-		public active: boolean,
-		public position: string,
-		public access_level: Number,
-		public user: User | null,
+		public birthday: string,
+		public user: User,
 	) {}
 }
