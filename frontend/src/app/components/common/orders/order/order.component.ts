f4a1149ab31a1c29ddd6aa53e66503645f93783b
Montaguti <5656679+Montaguti@users.noreply.github.com>
2025-05-09 17:17:24 -0300

    add base template to visualization

--- frontend/src/app/components/common/orders/order/order.component.ts
+++ frontend/src/app/components/common/orders/order/order.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-order',
-  standalone: true,
-  imports: [],
-  templateUrl: './order.component.html',
-  styleUrl: './order.component.css'
+	selector: 'app-order',
+	standalone: true,
+	imports: [],
+	templateUrl: './order.component.html',
+	styleUrl: './order.component.css',
 })
 export class OrderComponent {
-
+	order = {
+		id: 1,
+		client_id: 1,
+		employee_id: 1,
+		category_id: 1,
+		equipment_description: 'NOTEBOOK X',
+		deffect_description: 'NAO LIGA',
+		price: null,
+		created_at: '23/04/2025 10:15',
+		status: 'ABERTA',
+		order_actions: [
+			{
+				id: 1,
+				order_id: 1,
+				employee_id: 1,
+				created_at: '23/04/2025 10:15',
+				STATUS: 'ABERTA',
+			},
+		],
+	};
 }
