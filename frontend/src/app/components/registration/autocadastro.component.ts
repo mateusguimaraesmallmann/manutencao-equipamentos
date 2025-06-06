9ad43c743d78461a8685daa5f12f18ebf191f180
Montaguti <5656679+Montaguti@users.noreply.github.com>
2025-06-06 19:00:12 -0300

    wip - autocadastroForm integrating with the API

--- frontend/src/app/components/registration/autocadastro.component.ts
+++ frontend/src/app/components/registration/autocadastro.component.ts
@@ -1,54 +1,69 @@
 import { Component } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
-import {FormGroup } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 
+import { AuthService } from '../../services/index';
+
 @Component({
-  selector: 'app-autocadastro',
-  templateUrl: './autocadastro.component.html',
-  styleUrls: ['./autocadastro.component.css'],
-  standalone: true,
-  imports: [CommonModule, ReactiveFormsModule]
+	selector: 'app-autocadastro',
+	templateUrl: './autocadastro.component.html',
+	styleUrls: ['./autocadastro.component.css'],
+	standalone: true,
+	imports: [CommonModule, ReactiveFormsModule],
 })
-
 export class AutocadastroComponent {
-  
-  cadastroForm: FormGroup;
+	cadastroForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private http: HttpClient, private router: Router) {
-    this.cadastroForm = this.fb.group({
-      cpf: ['', [Validators.required]],
-      nome: ['', [Validators.required]],
-      email: ['', [Validators.required, Validators.email]],
-      telefone: ['', [Validators.required]],
-      cep: ['', [Validators.required]],
-      endereco: ['']
-    });
-  }
+	constructor(
+		private fb: FormBuilder,
+		private http: HttpClient,
+		private router: Router,
+		private authService: AuthService,
+	) {
+		this.cadastroForm = this.fb.group({
+			document: ['', [Validators.required]],
+			name: ['', [Validators.required]],
+			email: ['', [Validators.required, Validators.email]],
+			phone: ['', [Validators.required]],
+			zip_code: ['', [Validators.required]],
+			address: [''],
+		});
+	}
 
-  onCepChange() {
-    const cep = this.cadastroForm.get('cep')?.value;
-    if (cep.length === 8) {
-      this.http.get(`https://viacep.com.br/ws/${cep}/json/`).subscribe((data: any) => {
-        if (!data.erro) {
-          this.cadastroForm.patchValue({
-            endereco: `${data.logradouro}, ${data.bairro}, ${data.localidade} - ${data.uf}`
-          });
-        }
-      });
-    }
-  }
+	onCepChange() {
+		const zip_code = this.cadastroForm.get('zip_code')?.value;
+		if (zip_code.length === 8) {
+			this.http
+				.get(`https://viacep.com.br/ws/${zip_code}/json/`)
+				.subscribe((data: any) => {
+					if (!data.erro) {
+						this.cadastroForm.patchValue({
+							address: `${data.logradouro}, ${data.bairro}, ${data.localidade} - ${data.uf}`,
+						});
+					}
+				});
+		}
+	}
 
-  onSubmit() {
-    if (this.cadastroForm.valid) {
-      console.log('Dados do formulário:', this.cadastroForm.value);
-      alert('Cadastro realizado com sucesso!');
-    }
-  }
+	onSubmit() {
+		if (this.cadastroForm.valid) {
+			const registerData = this.cadastroForm.value;
 
-  voltarParaLogin() {
-    this.router.navigate(['/login']);
-  }
+			this.authService.registerClient(registerData).subscribe({
+				next: (response) => {
+					alert('Usuário cadastrado com sucesso');
+				},
+				error: (error) => {
+					console.log(error);
+				},
+			});
+		}
+	}
+
+	voltarParaLogin() {
+		this.router.navigate(['/login']);
+	}
 }
