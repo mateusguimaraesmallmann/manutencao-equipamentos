91c832124efede21f08f232171491bfa19705b5d
Montaguti <5656679+Montaguti@users.noreply.github.com>
2025-06-10 20:32:11 -0300

    add - crud to existing categories list component

--- frontend/src/app/components/category/category.component.ts
+++ frontend/src/app/components/category/category.component.ts
@@ -1,31 +1,60 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { Category } from '../../models';
-import { CategoryService } from '../../services/category.service';
+import { ReactiveFormsModule } from '@angular/forms';
 import { Observable } from 'rxjs';
+
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
+import { CategoryService } from '../../services/category.service';
+import { Category } from '../../models';
+import { CategoryDialogComponent } from './input.component';
 
 @Component({
 	selector: 'categories-tag',
 	standalone: true,
-	imports: [CommonModule],
+	imports: [ReactiveFormsModule, CommonModule],
 	templateUrl: './category.component.html',
 	styleUrl: './category.component.css',
 })
 export class CategoryComponent implements OnInit {
 	categories: Category[] = [];
 
-	constructor(private categoryService: CategoryService) {}
+	constructor(
+		private categoryService: CategoryService,
+		private activeModal: NgbModal,
+	) {}
 
 	ngOnInit(): void {
+		this.loadCategories();
+	}
+
+	loadCategories(): void {
 		this.categoryService.getCategories().subscribe({
-			next: (_) => {
+			next: (response) => {
 				this.categories = this.categoryService.categoriesCache;
 			},
 		});
 	}
 
 	newCategory() {
-		alert('Nova categoria');
+		this.activeModal.open(CategoryDialogComponent);
+	}
+
+	updateCategory(id: any) {
+		const modalRef = this.activeModal.open(CategoryDialogComponent);
+		modalRef.componentInstance.categoryId = id;
+
+		modalRef.result.then((response: any) => {
+			this.loadCategories();
+		});
+	}
+
+	deleteCategory(id: any) {
+		if (confirm(`Tem certeza que deseja excluir a categoria ${id}`)) {
+			this.categoryService.destroyCategory(id).subscribe(
+				(response) => this.loadCategories(),
+				(error) => console.log('Error deletando categoria:', error),
+			);
+		}
 	}
 }
