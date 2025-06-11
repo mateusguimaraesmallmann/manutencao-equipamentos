71c3cac4ecd9abe87f7b73f49318f8dbedd2aec1
Montaguti <5656679+Montaguti@users.noreply.github.com>
2025-06-10 23:25:30 -0300

    add - opening forms modal when clicked new employee button

--- frontend/src/app/components/employee/employee.component.ts
+++ frontend/src/app/components/employee/employee.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
 import { EmployeeService } from '../../services';
 import { Employee } from '../../models';
+import { EmployeeDialogComponent } from './input.compoment';
 
 @Component({
-	selector: 'app-employee',
 	standalone: true,
 	imports: [CommonModule],
 	templateUrl: './employee.component.html',
@@ -14,17 +16,38 @@ import { Employee } from '../../models';
 export class EmployeeComponent implements OnInit {
 	employees: Employee[] = [];
 
-	constructor(private employeeService: EmployeeService) {}
+	constructor(
+		private employeeService: EmployeeService,
+		private activeModal: NgbModal,
+	) {}
 
 	ngOnInit(): void {
-		this.employeeService.getEmployeesFull().subscribe({
+		this.employeeService.getEmployees().subscribe({
 			next: (response) => {
-				this.employees = response;
+				this.employees = this.employeeService.employeesCache;
 			},
 		});
 	}
 
 	newEmployee() {
-		alert('Novo funcionário');
+		this.activeModal.open(EmployeeDialogComponent);
 	}
+
+	//updateCategory(id: any) {
+	//	const modalRef = this.activeModal.open(CategoryDialogComponent);
+	//	modalRef.componentInstance.categoryId = id;
+
+	//	modalRef.result.then((response: any) => {
+	//		this.loadCategories();
+	//	});
+	//}
+
+	//deleteCategory(id: any) {
+	//	if (confirm(`Tem certeza que deseja excluir a categoria ${id}`)) {
+	//		this.categoryService.destroyCategory(id).subscribe(
+	//			(response) => this.loadCategories(),
+	//			(error) => console.log('Error deletando categoria:', error),
+	//		);
+	//	}
+	//}
 }
