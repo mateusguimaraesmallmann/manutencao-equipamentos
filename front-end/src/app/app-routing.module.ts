54c1f50468f496bea9f188acdd1e5e2006e0822b
Luiz Antônio Gonçalves <antonio.luiz@ufpr.br>
2025-08-17 23:14:46 -0300

    Create first structure of the project, sign-up page with components and route

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ClientSignUpComponent} from './pages/client-sign-up/client-sign-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'sign-up', component: ClientSignUpComponent, pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ front-end/src/app/pages/client-sign-up/componets/sign-up-form/sign-up-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-up-form',
+  standalone: true,
+  templateUrl: './sign-up-form.component.html',
+  styleUrl: './sign-up-form.component.css'
+})
+export class SignUpFormComponent {
+
+}
