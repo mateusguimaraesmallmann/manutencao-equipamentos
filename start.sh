84c9f3f89fb0ba0cb880d025dc0a538784cc18ae
Mateus Irineu Guimarães Mallmann <mateusmallmann2013@gmail.com>
2025-09-03 16:29:02 -0300

    Frontend removed from docker-compose, removed dockerfile from frontend and improved start.sh
    Frontend removed from docker-compose, removed dockerfile from frontend and improved start.sh

--- start.sh
+++ start.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
 
-echo "🚀 Iniciando build completo da aplicação..."
+echo "🚀 Iniciando build do backend da aplicação..."
 
 # 1. Parar e remover containers e redes
 echo "🧹 Parando containers..."
-docker compose down --remove-orphans #se quiser REMOVER volumes (reset total do banco), troque a linha por: docker compose down -v --remove-orphans
+docker compose down --remove-orphans # -> REMOVE volumes (reset total do banco), troque a linha por: docker compose down -v --remove-orphans
 
-# 2. Build do front-end
-#echo "🎨 Realizando build do front-end..."
-#cd ./front-end || exit 1
-#npm install
-#npm run build
-#cd ..
-
-# 3. Subir containers com build forçado
+# 2. Subir containers com build forçado
 echo "🐳 Subindo os containers com Docker Compose..."
 docker compose up --build -d
 
-echo "✅ Aplicação Manutenção de Equipamentos iniciada com sucesso!"
+echo "✅ Backend da Aplicação Manutenção de Equipamentos iniciada com sucesso!"
 echo "   • Back-end:  http://localhost:8080"
-echo "   • Front-end: http://localhost:4200"
