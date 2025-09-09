21dfba12c163bd3a0eab84c7b25a9cb5ec89fe9d
Luiz Antônio Gonçalves <antonio.luiz@ufpr.br>
2025-09-09 13:34:59 -0300

    Refactor footer styles and update images and galleria components with initial implementations

---
+++ front-end/src/app/pages/inicial/components/galleria/galleria.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Carousel } from "primeng/carousel";
+
+@Component({
+  selector: 'app-galleria',
+  imports: [Carousel],
+  templateUrl: './galleria.component.html',
+  styleUrl: './galleria.component.css'
+})
+export class GalleriaComponent {
+
+}
