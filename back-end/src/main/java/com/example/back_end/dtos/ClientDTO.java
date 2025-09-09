b80861c08199d68b9aec755ec8014bafd367513a
Mateus Irineu Guimarães Mallmann <mateusmallmann2013@gmail.com>
2025-09-09 18:28:48 -0300

    continuation of the implementation of self-registration
    continuation of the implementation of self-registration

--- back-end/src/main/java/com/example/back_end/dtos/ClientDTO.java
+++ back-end/src/main/java/com/example/back_end/dtos/ClientDTO.java
@@ -1,3 +1,14 @@
 package com.example.back_end.dtos;
 
-public record ClientDTO () {}
+public record ClientDTO(
+    Long id,
+    String cpf,
+    String nome,
+    String email,
+    String telefone,
+    String cep,
+    String numero,
+    String complemento,
+    String cidade,
+    String estado
+) {}
--- back-end/src/main/java/com/example/back_end/repositorys/ClientRepository.java
+++ back-end/src/main/java/com/example/back_end/repositorys/ClientRepository.java
@@ -1,9 +1,17 @@
 package com.example.back_end.repositorys;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.back_end.models.ClienteProfile;
 
 public interface ClientRepository extends JpaRepository<ClienteProfile, Long>{
+
+    Optional<ClienteProfile> findByCpf(String cpf);
+    Optional<ClienteProfile> findByEmail(String email);
+
+    boolean existsByCpf(String cpf);
+    boolean existsByEmail(String email);
     
 }
