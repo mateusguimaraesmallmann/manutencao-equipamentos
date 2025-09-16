88c727bfe747796d5fb4b9233e7a7e758366b333
Luiz Antônio Gonçalves <antonio.luiz@ufpr.br>
2025-09-16 20:30:36 -0300

    removed sing-up component

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { InicialComponent } from './pages/inicial/inicial.component';
-import { ClientSignUpComponent } from './pages/client-sign-up/client-sign-up.component';
 import { LoginComponent } from './pages/login/login.component';
 import { AutocadastroComponent } from './pages/autocadastro/autocadastro.component';
 import { ClienteComponent } from './pages/pagina-cliente/pagina-cliente.component';
@@ -10,7 +9,6 @@ export const routes: Routes = [
 
   { path: '', pathMatch: 'full', redirectTo: 'login' },
   { path: 'inicial', component: InicialComponent },
-  { path: 'sign-up', component: ClientSignUpComponent },
   { path: 'login', component: LoginComponent },
   { path: 'autocadastro', component: AutocadastroComponent },
   { path: 'pagina-cliente', component: ClienteComponent },
