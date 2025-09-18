3fafa375dfcec6bc995922e6f03ea90f82f7b899
Lucas Schip <lucasschip@gmail.com>
2025-09-18 17:41:38 -0300

    Correção problema com as rotas

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -3,59 +3,33 @@ import { InicialComponent } from './pages/cliente/inicial/inicial.component';
 import { LoginComponent } from './pages/login/login.component';
 import { AutocadastroComponent } from './pages/cliente/autocadastro/autocadastro.component';
 import { ClienteComponent } from './pages/cliente/pagina-cliente/pagina-cliente.component';
+import { SolicitacaoManutencaoComponent } from './pages/cliente/solicitacao-manutencao/solicitacao-manutencao.component';
 import { FuncionarioInicialComponent } from './pages/funcionario/funcionario-inicial/funcionario-inicial.component';
+import { EfetuarOrcamentoComponent } from './pages/funcionario/efetuar-orcamento/efetuar-orcamento.component';
+import { EfetuarManutencaoComponent } from './pages/funcionario/efetuar-manutencao/efetuar-manutencao.component';
+import { EquipamentosComponent } from './pages/funcionario/equipamentos/equipamentos.component';
+import { FuncionariosComponent } from './pages/funcionario/funcionarios/funcionarios.component';
+import { RelatoriosComponent } from './pages/funcionario/relatorios/relatorios.component';
+
 import { funcionarioGuard } from './guards/funcionario.guard';
 
 export const routes: Routes = [
 
-  //{ path: '', pathMatch: 'full', redirectTo: 'login' },
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
+
   { path: 'inicial', component: InicialComponent },
   { path: 'login', component: LoginComponent },
   { path: 'autocadastro', component: AutocadastroComponent },
   { path: 'pagina-cliente', component: ClienteComponent },
-  {
-    path: 'solicitacao-manutencao',
-    loadComponent: () =>
-      import('./pages/cliente/solicitacao-manutencao/solicitacao-manutencao.component')
-        .then(m => m.SolicitacaoManutencaoComponent)
-  },
+  { path: 'solicitacao-manutencao', component: SolicitacaoManutencaoComponent },
+
   { path: 'funcionario', component: FuncionarioInicialComponent },
-  {
-    path: 'orcamento/:id',
-    canMatch: [funcionarioGuard],
-    loadComponent: () =>
-      import('./pages/funcionario/efetuar-orcamento/efetuar-orcamento.component')
-        .then(m => m.EfetuarOrcamentoComponent)
-  },
-  {
-    path: 'manutencao/:id',
-    canMatch: [funcionarioGuard],
-    loadComponent: () =>
-      import('./pages/funcionario/efetuar-manutencao/efetuar-manutencao.component')
-        .then(m => m.EfetuarManutencaoComponent)
-  },
-  { path: '**', redirectTo: 'login' },
-  {
-    path: 'equipamentos',
-    canMatch: [funcionarioGuard],
-    loadComponent: () =>
-      import('./pages/funcionario/equipamentos/equipamentos.component')
-        .then(m => m.EquipamentosComponent)
-  },
-  {
-    path: 'funcionarios',
-    canMatch: [funcionarioGuard],
-    loadComponent: () =>
-      import('./pages/funcionario/funcionarios/funcionarios.component')
-        .then(m => m.FuncionariosComponent)
-  },
-  {
-    path: 'relatorios',
-    canMatch: [funcionarioGuard],
-    loadComponent: () =>
-      import('./pages/funcionario/relatorios/relatorios.component')
-        .then(m => m.RelatoriosComponent)
-  },
-  { path: '**', redirectTo: 'login' },
-  
+  { path: 'orcamento/:id', component: EfetuarOrcamentoComponent },
+  { path: 'manutencao/:id', component: EfetuarManutencaoComponent },
+  { path: 'equipamentos', component: EquipamentosComponent },
+  { path: 'funcionarios', component: FuncionariosComponent, canMatch: [funcionarioGuard] },
+  { path: 'relatorios', component: RelatoriosComponent },
+
+  { path: '**', redirectTo: 'login' }
 ];
+
