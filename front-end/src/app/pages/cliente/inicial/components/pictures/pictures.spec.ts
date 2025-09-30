40834d7fe3ef974206b69f3cb307a9fe8bcce703
luizantonio0 <antonio.luiz@ufpr.br>
2025-09-30 20:25:13 -0300

    add initial components for pictures, posts, and videos with basic templates and tests

---
+++ front-end/src/app/pages/cliente/inicial/components/pictures/pictures.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Pictures } from './pictures';
+
+describe('Pictures', () => {
+  let component: Pictures;
+  let fixture: ComponentFixture<Pictures>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [Pictures]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(Pictures);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ front-end/src/app/pages/cliente/inicial/components/pictures/pictures.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pictures',
+  imports: [],
+  templateUrl: './pictures.html',
+  styleUrl: './pictures.css'
+})
+export class Pictures {
+
+}
