e84929b5ecc950be5fac34876175c78c6f0e22c3
luizantonio0 <antonio.luiz@ufpr.br>
2025-10-13 21:02:21 -0300

    Implementação do componente de galeria de imagens com integração de dados e estilos

--- front-end/src/app/pages/cliente/inicial/components/galleria/components/images/images.component.ts
+++ front-end/src/app/pages/cliente/inicial/components/galleria/components/images/images.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+interface Image {
+    url: string;
+    title: string;
+}
 
 @Component({
   selector: 'app-images',
@@ -6,6 +11,38 @@ import { Component } from '@angular/core';
   templateUrl: './images.component.html',
   styleUrl: './images.component.css'
 })
-export class ImagesComponent {
+export class ImagesComponent implements OnInit {
+    images: Image[] = []
+    responsiveOptions: any[] = [
+        {
+            breakpoint: '1024px',
+            numVisible: 5
+        },
+        {
+            breakpoint: '768px',
+            numVisible: 3
+        },
+        {
+            breakpoint: '560px',
+            numVisible: 1
+        }
+    ]
 
+    ngOnInit() {
+        this.images = [
+            {
+                url: 'assets/images/image1.jpg',
+                title: 'Imagem 1'
+            },
+            {
+                url: 'assets/images/image2.jpg',
+                title: 'Imagem 2'
+            },
+            {
+                url: 'assets/images/image3.jpg',
+                title: 'Imagem 3'
+            }
+            
+        ]
+    }
 }
