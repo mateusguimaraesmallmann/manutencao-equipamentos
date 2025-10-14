9dd04082f21ca15e8c5a2126d424f1b5a236e24f
Pedro Tonial Loureiro Guerreiro de Lemos <pedrotlgl@gmail.com>
2025-10-14 15:53:45 -0300

    alteração infos

--- front-end/src/app/pages/cliente/perfil-cliente/perfil-cliente.component.ts
+++ front-end/src/app/pages/cliente/perfil-cliente/perfil-cliente.component.ts
@@ -21,11 +21,35 @@ export class PerfilClienteComponent {
     { id: 3, equipamento: 'Monitor LG 24"', status: 'Aguardando peças', data: '06/10/2025' }
   ];
 
+  avaliacoes = [
+    { nota: 5, comentario: 'Serviço excelente e rápido!' },
+    { nota: 4, comentario: 'Atendimento bom, mas o prazo poderia ser menor.' },
+    { nota: 3, comentario: 'Tive que retornar duas vezes, mas resolveram.' }
+  ];
+
+  novaAvaliacao = { nota: 0, comentario: '' };
+
   editarDados() {
     alert('Função de edição em desenvolvimento!');
   }
 
   contarEquipamentosEmAberto(): number {
     return this.historicoManutencoes.filter(m => m.status !== 'Concluído').length;
+  }
+
+  calcularMediaAvaliacoes(): number {
+    if (this.avaliacoes.length === 0) return 0;
+    const soma = this.avaliacoes.reduce((acc, a) => acc + a.nota, 0);
+    return +(soma / this.avaliacoes.length).toFixed(1);
+  }
+
+  adicionarAvaliacao() {
+    if (this.novaAvaliacao.nota > 0 && this.novaAvaliacao.comentario.trim() !== '') {
+      this.avaliacoes.push({ ...this.novaAvaliacao });
+      this.novaAvaliacao = { nota: 0, comentario: '' };
+      alert('Avaliação adicionada com sucesso!');
+    } else {
+      alert('Preencha a nota e o comentário antes de enviar.');
+    }
   }
 }
